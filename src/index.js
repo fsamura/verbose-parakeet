0c189f34d445f7121f2109ae09bf58395843d7a5
Foday Samura <f_samura@salemstate.edu>
2022-05-22 06:57:40 -0400

    Feat: A new feature add navbar component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// pre-defind files
+import "jquery";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "popper.js/dist/umd/popper";
+import "font-awesome/css/font-awesome.css";
+// users defind files
+// import App from './App';
+import "./index.css";
+import NavBar from './NavBar';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <NavBar />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
